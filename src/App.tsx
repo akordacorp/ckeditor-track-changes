f468a3ea9f1a44b2b4b30ec5858a401f7a02c9a2
Emanuel Alejandro Cuevas <emaacaves@gmail.com>
2020-04-23 13:04:12 -0300

    fix: delete in insert

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,17 @@ const App: React.FC = () => {
           extraPlugins: 'liter',
           lite: {
             userId: 191,
-            userName: 'Matt Meiske'
-          }
+            userName: 'Matt Meiske',
+          },
         }}
         data={`<div>
-          <p>hello world</p>
-          <p>
-            one two three
-            <span class="comment-start"></span> testing <span></span>
-            <span class="comment-end"></span>
-            four fix six
-          </p>
-        <div>`}
+        <p>
+        <ins data-id="1" class="ice-ins ice-cts-2"><span>The quick <b>brown</b> fox <i>jumps</i> over the lazy dog</span></ins>
+        </p>
+        <p>
+          Text before ins <ins data-id="1" class="ice-ins ice-cts-2"><span data-last-name="meiske">The comment starts here→<span data-w-id="1" data-track-changes-ignore="true" cke-editable="false" unselectable="true" contenteditable="false" start="true" class="comment-marker comment-start"></span><span data-c-id="s1 " class="marker-comment" removable="true">I am a comment</span><img data-c-id="i1" class="marker-image" src="/assets/src/media/img/font-comment.png?440e7fee" data-track-changes-ignore="true" data-cke-real-element-type="annotation" cke-editable="false" contenteditable="false"><span data-w-id="1" data-track-changes-ignore="true" cke-editable="false" contenteditable="false" end="true" class="comment-end comment-marker"></span>←and ends there.</span></ins> Text after ins
+        </p>
+      <div>`}
         onBeforeLoad={(CKEDITOR: any) => {
           CKEDITOR.disableAutoInline = true;
           CKEDITOR.dtd.$removeEmpty.ins = 0;
